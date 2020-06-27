dbf8e69a492630b7c56d416a9d931740060c48a7
ivandi1980 <ivandi.djoh@gmail.com>
2020-06-28 02:10:42 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## BLOG APP BACKEND
+
+## About
+
+This is a backend Rest Api for `Blog App` using just `Native PHP` to support the `Ract js` on the frontend.
+The purpose of this repo is just to do some practice using PHP.
---
+++ models/Category.php
@@ -0,0 +1,149 @@
+<?php
+  class Category {
+    // DB Stuff
+    private $conn;
+    private $table = 'categories';
+
+    // Properties
+    public $id;
+    public $name;
+    public $created_at;
+
+    // Constructor with DB
+    public function __construct($db) {
+      $this->conn = $db;
+    }
+
+    // Get categories
+    public function read() {
+      // Create query
+      $query = 'SELECT
+        id,
+        name,
+        created_at
+      FROM
+        ' . $this->table . '
+      ORDER BY
+        created_at DESC';
+
+      // Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Execute query
+      $stmt->execute();
+
+      return $stmt;
+    }
+
+    // Get Single Category
+  public function read_single(){
+    // Create query
+    $query = 'SELECT
+          id,
+          name
+        FROM
+          ' . $this->table . '
+      WHERE id = ?
+      LIMIT 0,1';
+
+      //Prepare statement
+      $stmt = $this->conn->prepare($query);
+
+      // Bind ID
+      $stmt->bindParam(1, $this->id);
+
+      // Execute query
+      $stmt->execute();
+
+      $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+      // set properties
+      $this->id = $row['id'];
+      $this->name = $row['name'];
+  }
+
+  // Create Category
+  public function create() {
+    // Create Query
+    $query = 'INSERT INTO ' .
+      $this->table . '
+    SET
+      name = :name';
+
+  // Prepare Statement
+  $stmt = $this->conn->prepare($query);
+
+  // Clean data
+  $this->name = htmlspecialchars(strip_tags($this->name));
+
+  // Bind data
+  $stmt-> bindParam(':name', $this->name);
+
+  // Execute query
+  if($stmt->execute()) {
+    return true;
+  }
+
+  // Print error if something goes wrong
+  printf("Error: $s.\n", $stmt->error);
+
+  return false;
+  }
+
+  // Update Category
+  public function update() {
+    // Create Query
+    $query = 'UPDATE ' .
+      $this->table . '
+    SET
+      name = :name
+      WHERE
+      id = :id';
+
+  // Prepare Statement
+  $stmt = $this->conn->prepare($query);
+
+  // Clean data
+  $this->name = htmlspecialchars(strip_tags($this->name));
+  $this->id = htmlspecialchars(strip_tags($this->id));
+
+  // Bind data
+  $stmt-> bindParam(':name', $this->name);
+  $stmt-> bindParam(':id', $this->id);
+
+  // Execute query
+  if($stmt->execute()) {
+    return true;
+  }
+
+  // Print error if something goes wrong
+  printf("Error: $s.\n", $stmt->error);
+
+  return false;
+  }
+
+  // Delete Category
+  public function delete() {
+    // Create query
+    $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+
+    // Prepare Statement
+    $stmt = $this->conn->prepare($query);
+
+    // clean data
+    $this->id = htmlspecialchars(strip_tags($this->id));
+
+    // Bind Data
+    $stmt-> bindParam(':id', $this->id);
+
+    // Execute query
+    if($stmt->execute()) {
+      return true;
+    }
+
+    // Print error if something goes wrong
+    printf("Error: $s.\n", $stmt->error);
+
+    return false;
+    }
+  }
